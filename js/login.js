79901e3cd4a4166995dacac4c70422d46b7a87c6
Sabbir-663 <ahamedsabbir663@gmail.com>
2024-09-22 19:23:42 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,11 @@
+
+document.getElementById('button-login').addEventListener('click',function(event){
+    console.log("login button clicked");
+    event.preventDefault();
+
+    const phoneNumber=document.getElementById('phone-number').value ;
+    console.log(phoneNumber);
+    
+    
+})
+
